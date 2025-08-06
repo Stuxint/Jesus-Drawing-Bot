a2acd9841a93b732d97aadde93daf9fec5cfa142
Stuxint <160317550+Stuxint@users.noreply.github.com>
2025-08-06 17:13:19 +0400

    Add files via upload

---
+++ Jesus Drawing Bot.py
@@ -0,0 +1,223 @@
+#--------------MADE BOT WHICH CAN DRAW JESUS!!!!!!!----------------------------------
+
+
+import pyautogui
+import time
+
+pyautogui.hotkey('win', 'run')
+time.sleep(1)
+pyautogui.typewrite('paint')
+time.sleep(3)
+pyautogui.hotkey('enter')
+
+time.sleep(5)
+pyautogui.moveTo(935,640)
+
+#DRAWING FACE
+    #STEP 1
+time.sleep(2)
+pyautogui.dragTo(935,570)
+    #STEP 2
+time.sleep(1)
+pyautogui.dragTo(1015,570)
+    #STEP 3
+time.sleep(1)
+pyautogui.dragTo(1015,640)
+    #STEP 4
+time.sleep(1)
+pyautogui.dragTo(935,640)
+
+#DRAWING NECK
+time.sleep(1)
+pyautogui.moveTo(945,640)
+time.sleep(1)
+pyautogui.dragTo(945,650)
+time.sleep(1)
+pyautogui.dragTo(1005,650)
+time.sleep(1)
+pyautogui.dragTo(1005,640)
+
+#DRAWING ARMS AND BODY
+    #STEP 1
+time.sleep(1)
+pyautogui.moveTo(945,650)
+pyautogui.dragTo(915,650)
+
+pyautogui.dragTo(915,740)
+
+    #STEP 2
+time.sleep(1)
+pyautogui.dragTo(935,740)
+
+    #STEP 3
+time.sleep(1)
+pyautogui.dragTo(935,680)
+
+    #STEP 4
+time.sleep(1)
+pyautogui.dragTo(945,680)
+
+    #STEP 5
+time.sleep(1)
+pyautogui.dragTo(945,770)
+
+    #STEP 6
+time.sleep(1)
+pyautogui.dragTo(1005,770)
+
+    #STEP 7
+time.sleep(1)
+pyautogui.dragTo(1005,680)
+
+    #STEP 7
+time.sleep(1)
+pyautogui.dragTo(1015,680)
+
+    #STEP 8
+time.sleep(1)
+pyautogui.dragTo(1015,740)
+
+    #STEP 9
+time.sleep(1)
+pyautogui.dragTo(1035,740)
+
+    #STEP 10
+time.sleep(1)
+pyautogui.dragTo(1035,650)
+
+    #STEP 11
+time.sleep(1)
+pyautogui.dragTo(1005,650)
+
+#DRAWING Sash
+    #STEP 1
+time.sleep(1)
+pyautogui.moveTo(945,720)
+
+    #STEP 2
+time.sleep(1)
+pyautogui.dragTo(1005,720)
+
+    #STEP 3
+time.sleep(1)
+pyautogui.moveTo(945,740)
+
+    #STEP 4
+time.sleep(1)
+pyautogui.dragTo(1005,740)
+
+#DRAWING SMILE
+time.sleep(1)
+pyautogui.moveTo(945,610)
+
+    #STEP 2
+time.sleep(1)
+pyautogui.dragTo(945,630)
+
+    #STEP 3
+time.sleep(1)
+pyautogui.dragTo(1005,630)
+
+    #STEP 4
+time.sleep(1)
+pyautogui.dragTo(1005,610)
+
+#DRAWING EYES
+time.sleep(1)
+pyautogui.hotkey('tab')
+
+    #STEP 2
+time.sleep(1)
+pyautogui.hotkey('up')
+pyautogui.hotkey('up')
+pyautogui.hotkey('up')
+pyautogui.hotkey('up')
+pyautogui.hotkey('up')
+pyautogui.hotkey('up')
+pyautogui.hotkey('up')
+pyautogui.hotkey('up')
+pyautogui.hotkey('up')
+
+    #STEP 3
+time.sleep(1)
+pyautogui.moveTo(955,585)
+pyautogui.click()
+
+    #STEP 4
+time.sleep(1)
+pyautogui.moveTo(995,585)
+pyautogui.click()
+
+#DRAWING HAIR
+time.sleep(1)
+pyautogui.hotkey('tab')
+
+    #STEP 2
+time.sleep(1)
+pyautogui.hotkey('up')
+pyautogui.hotkey('up')
+pyautogui.hotkey('up')
+pyautogui.hotkey('up')
+pyautogui.hotkey('up')
+pyautogui.hotkey('up')
+pyautogui.hotkey('up')
+pyautogui.hotkey('up')
+
+    #STEP 3
+time.sleep(1)
+pyautogui.moveTo(925,630)
+
+    #STEP 4
+time.sleep(2)
+pyautogui.dragTo(925,560)
+
+    #STEP 5
+time.sleep(1)
+pyautogui.dragTo(1025,560)
+
+    #STEP 6
+time.sleep(1)
+pyautogui.dragTo(1025,630)
+
+
+#PAINTING SASH AND FACE AND NECK
+    #STEP 1
+time.sleep(1)
+pyautogui.moveTo(1165, 260)
+pyautogui.click()
+
+    #STEP 2
+pyautogui.moveTo(655, 260)
+pyautogui.click()
+
+    #STEP 
+time.sleep(1)
+pyautogui.moveTo(955,730)
+
+time.sleep(1)
+pyautogui.dragTo(955,730)
+pyautogui.click()
+
+    #STEP 1
+time.sleep(1)
+pyautogui.moveTo(1125, 290)
+pyautogui.click()
+
+    #STEP 
+time.sleep(1)
+pyautogui.moveTo(945,600)
+
+time.sleep(1)
+pyautogui.dragTo(945,600)
+pyautogui.click()
+
+    #STEP 
+time.sleep(1)
+pyautogui.moveTo(955,645)
+
+time.sleep(1)
+pyautogui.dragTo(955,645)
+pyautogui.click()
+
+
+time.sleep(20)
